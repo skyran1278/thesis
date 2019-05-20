a93074242dfd0558ea3444ffac69905d68f8f12a
skyran1278 <skyran1278@gmail.com>
2019-05-20 23:44:40 +0800

    unit

--- EvaluationReport/evaluation/app.py
+++ EvaluationReport/evaluation/app.py
@@ -11,11 +11,15 @@ from evaluation.plot_design import PlotDesign
 
 def etabs_to_addedld_sol(data):
     plt.figure()
+
     data.zero_line()
+    data.min_line()
 
     data.etabs_demand_line('blue')
 
     data.demand_line('green')
+    plt.xlabel('Length(m)')
+    plt.ylabel('As($m^2$)')
 
 
 def v_demand(data):
@@ -36,6 +40,8 @@ def v_demand(data):
 
 def tradition_flow(data):
     plt.figure()
+    plt.xlabel('Length(m)')
+    plt.ylabel('As($m^2$)')
     data.zero_line()
 
     data.tradition_boundary_line()
@@ -49,6 +55,8 @@ def tradition_flow(data):
 def linearcut_flow(data):
     plt.figure()
     data.zero_line()
+    plt.xlabel('Length(m)')
+    plt.ylabel('As($m^2$)')
     data.min_line()
     data.boundary_line()
 
@@ -107,7 +115,7 @@ def main():
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    input_file = '../data/20190520 121213 SmartCut.xlsx'
+    input_file = '../data/20190518 170312 SmartCut LowSeismic 4Floor 12M.xlsx'
 
     path = f'{script_dir}/{input_file}'
 
@@ -117,21 +125,23 @@ def main():
 
     # for index in (208, 1380, 1532, 124, 144, 1068, 72):
     #     # for index in (8, 1, 4, 40, 16, 24):
-    for index in range(0, data.design.get_len(), 4):
-        # for index in (0, 620, 692, 764, 1472, 1324, 1204, 1332):
-        data.put_index(index)
+    # for index in range(0, data.design.get_len(), 4):
+    # for index in (0, 620, 692, 764, 1472, 1324, 1204, 1332):
+    data.put_index(0)
 
-    # plt.figure()
-    # data.zero_line()
+    plt.figure()
+    data.zero_line()
+    plt.xlabel('Length(m)')
+    plt.ylabel('As($m^2$)')
 
     # data.etabs_demand_line('blue')
     # # data.rebar_line('green')
-    # data.demand_line('green')
-    # data.min_line()
-    # data.boundary_line()
-    # data.boundary_line(0.45)
+    data.demand_line('green')
+    data.min_line()
+    data.boundary_line()
+    data.boundary_line(0.45)
 
-        v_demand(data)
+    # v_demand(data)
     # plt.figure()
     # plt.xlabel('Length(m)')
     # plt.ylabel(r'As($m^2$)')
@@ -144,10 +154,10 @@ def main():
 
     # data.rebar_line('gray', '傳統斷筋')
     # data.rebar_line('blue', '多點斷筋')
-    # etabs_to_addedld_sol(data)
+    etabs_to_addedld_sol(data)
     # tradition_flow(data)
-    # linearcut_flow(data)
-        compare_linearcut_to_tradition(data)
+    linearcut_flow(data)
+    # compare_linearcut_to_tradition(data)
 
     plt.show()
 
