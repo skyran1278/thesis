109e9195c0e0c9b2485868ca6b3bf028e9a76c6a
skyran1278 <skyran1278@gmail.com>
2019-05-25 20:43:11 +0800

    fix divergence chance

--- NonlinearReport/pushover/get_yielding_point.m
+++ NonlinearReport/pushover/get_yielding_point.m
@@ -5,7 +5,7 @@ function [dy, ay] = get_yielding_point(elastic_sd, elastic_sa, capacity_sd, capa
 
     capacity_area = trapz([capacity_sd(capacity_sd < d_star), d_star], [capacity_sa(capacity_sd < d_star), a_star]);
 
-    while abs(bilinear_area - capacity_area) > 1e-3
+    while abs(bilinear_area - capacity_area) > 1e-2
         dy = dy + 1e-4;
         ay = linear_interpolate(dy, elastic_sd(1), elastic_sd(2), elastic_sa(1), elastic_sa(2));
 
