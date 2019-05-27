ce9b319bbc5dafa418534887ace24292bb8ca945
skyran1278 <skyran1278@gmail.com>
2019-05-28 07:21:05 +0800

    low412 multi

--- NonlinearReport/config.py
+++ NonlinearReport/config.py
@@ -21,7 +21,30 @@ highseismic_4floor_6m = {
     'tradition': 'D:/GitHub/thesis/Models/HighSeismic 4Floor 6M/Conservative/Tradition/story_drifts.xlsx',
 }
 
-midseismic_12floor_12m = {
+lowseismic_12floor_12m = {
+    'scaled_facotrs': {
+        'DBE': 0.297,
+        'MCE': 0.396,
+    },
+    'earthquakes': {
+        'RSN725_SUPER.B_B-POE360': {'sa': 0.548, 'pga': 0.463},
+        'RSN900_LANDERS_YER270': {'sa': 0.448, 'pga': 0.224},
+        'RSN953_NORTHR_MUL279': {'sa': 0.648, 'pga': 0.343},
+        'RSN960_NORTHR_LOS000': {'sa': 0.388, 'pga': 0.426},
+        'RSN1111_KOBE_NIS000': {'sa': 0.296, 'pga': 0.483},
+        'RSN1116_KOBE_SHI000': {'sa': 0.486, 'pga': 0.336},
+        'RSN1148_KOCAELI_ARE090': {'sa': 0.140, 'pga': 0.157},
+        'RSN1158_KOCAELI_DZC180': {'sa': 0.343, 'pga': 0.248},
+        'RSN1602_DUZCE_BOL090': {'sa': 0.791, 'pga': 0.574},
+        'RSN1633_MANJIL_ABBAR--T': {'sa': 0.501, 'pga': 0.460},
+        'RSN1787_HECTOR_HEC090': {'sa': 0.402, 'pga': 0.343},
+    },
+    'story': 'D:/GitHub/thesis/Models/LowSeismic 4Floor 12M/Multi/story.xlsx',
+    'multi': 'D:/GitHub/thesis/Models/LowSeismic 4Floor 12M/Multi/story_drifts.xlsx',
+    'tradition': '',
+}
+
+midseismic_12floor_9m = {
     'scaled_facotrs': {
         'DBE': 0.264,
         'MCE': 0.32,
