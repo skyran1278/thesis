fa40255b1fe8751e6d11fcdff5168fdc3188a9ff
skyran1278 <skyran1278@gmail.com>
2019-06-06 12:48:35 +0800

    mid129 nonlinear data

--- EvaluationReport/evaluation/app.py
+++ EvaluationReport/evaluation/app.py
@@ -177,32 +177,32 @@ def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     data1 = PlotDesign(
-        f'{script_dir}/../data/HighSeismic 4Floor 12M.xlsx')
+        f'{script_dir}/../data/HighSeismic 4Floor 6M.xlsx')
     data2 = PlotDesign(
-        f'{script_dir}/../data/HighSeismic 4Floor 12M #10.xlsx')
+        f'{script_dir}/../data/HighSeismic 4Floor 9M.xlsx')
     data3 = PlotDesign(
         f'{script_dir}/../data/HighSeismic 4Floor 12M.xlsx')
 
-    data1.put_index(12)
-    data2.put_index(12)
-    data3.put_index(0)
+    data1.put_index(24)
+    data2.put_index(24)
+    data3.put_index(24)
 
-    plt.figure()
-    data1.zero_line()
-    data1.boundary_line(1/3)
+    # plt.figure()
+    # data1.zero_line()
+    # data1.boundary_line(1/3)
 
-    data1.etabs_demand_line('gray')
-    data1.add_ld_line('gray', label='Consider $l_d$')
-    data2.add_ld_line('gray', label='Consider $l_d$')
+    # data1.etabs_demand_line('gray')
+    # data1.add_ld_line('gray', label='Consider $l_d$')
+    # data2.add_ld_line('gray', label='Consider $l_d$')
 
-    line2 = data1.rebar_line('orange', '傳統斷筋')
-    line3 = data1.rebar_line('blue', '多點斷筋')
-    line2 = data2.rebar_line('red', '傳統斷筋')
-    line3 = data2.rebar_line('green', '多點斷筋')
+    # line2 = data1.rebar_line('orange', '傳統斷筋')
+    # line3 = data1.rebar_line('blue', '多點斷筋')
+    # line2 = data2.rebar_line('red', '傳統斷筋')
+    # line3 = data2.rebar_line('green', '多點斷筋')
 
-    plt.xlabel('Length(m)')
-    plt.ylabel('As($m^2$)')
-    plt.subplots_adjust(left=0.15)
+    # plt.xlabel('Length(m)')
+    # plt.ylabel('As($m^2$)')
+    # plt.subplots_adjust(left=0.15)
 
     # to_excel_orderby_effect(
     #     data1,
@@ -215,12 +215,12 @@ def main():
     # v_multicut_compare_tradition(data3)
     # tradition_flow(data1)
     # multicut_flow(data1)
-    # multicut_compare_tradition(data1)
+    multicut_compare_tradition(data1)
     # tradition_flow(data2)
 
     # etabs_to_addedld_sol(data2)
-    # multicut_compare_tradition(data2)
-    # multicut_compare_tradition(data3)
+    multicut_compare_tradition(data2)
+    multicut_compare_tradition(data3)
 
     # v_workflow(data1)
 
