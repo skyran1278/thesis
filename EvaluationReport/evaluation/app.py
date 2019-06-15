3d0cb3cb3a12a07115646c627aca13c2b039db96
skyran1278 <skyran1278@gmail.com>
2019-06-16 06:53:28 +0800

    整理結果

--- EvaluationReport/evaluation/app.py
+++ EvaluationReport/evaluation/app.py
@@ -345,7 +345,7 @@ def f4_9():
     data2.put_index(24)
     data3.put_index(24)
 
-    plt.figure(figsize=(6.4, 8))
+    plt.figure(figsize=(6.4, 8.6))
 
     plt.subplot(3, 1, 1)
     data1.zero_line()
@@ -654,9 +654,7 @@ def main():
     """
     f4_9()
 
-    # script_dir = os.path.dirname(os.path.abspath(__file__))
-
-    datas = [
+    data9 = [
         'D:/GitHub/thesis/Models/HighSeismic 4Floor 6M/Conservative/20190614 154022 Cut 2.xlsx',
         'D:/GitHub/thesis/Models/MidSeismic 4Floor 6M/20190614 154236 Cut 2.xlsx',
         'D:/GitHub/thesis/Models/LowSeismic 4Floor 6M/20190614 154117 Cut 2.xlsx',
@@ -668,69 +666,37 @@ def main():
         'D:/GitHub/thesis/Models/LowSeismic 4Floor 12M/20190615 103351 Cut 2.xlsx'
     ]
 
-    # data1 = PlotDesign(
+    data2 = [
+        'D:/GitHub/thesis/Models/MidSeismic 12Floor 9M/20190614 170742 Cut 2.xlsx',
+        'D:/GitHub/thesis/Models/MidSeismic 20Floor 9M/20190614 170819 Cut 2.xlsx'
+    ]
+
+    # script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # data = PlotDesign(
     #     'D:/GitHub/thesis/Models/LowSeismic 4Floor 12M/20190615 103351 Cut 2.xlsx')
-    # for index in range(0, data1.design.get_len(), 4):
-    #     data1.put_index(index)
-    #     multicut_flow(data1)
-    #     # tradition_flow(data1)
-    #     multicut_compare_tradition(data1)
+    # for index in range(0, data.design.get_len(), 4):
+    #     data.put_index(index)
+    #     etabs_to_addedld_sol(data)
+    #     tradition_flow(data)
+    #     multicut_flow(data)
+    #     multicut_compare_tradition(data)
 
-    # v_workflow(data1)
-    # for data in datas:
-    #     data1 = PlotDesign(data)
-    #     # for index in range(0, data1.design.get_len(), 24):
-    #     data1.put_index(0)
-    #     multicut_flow(data1)
-    #     data1 = PlotDesign(data)
-    # # data2 = PlotDesign(
-    # #     f'{script_dir}/../data/MidSeismic 4Floor 9M.xlsx')
-    # # data3 = PlotDesign(
-    # #     f'{script_dir}/../data/LowSeismic 4Floor 9M.xlsx')
+    #     v_workflow(data)
+    #     v_multicut_compare_tradition(data)
 
-    # data2.put_index(24)
-    # data3.put_index(24)
+    # etabs_to_addedld_sol(data)
+    # tradition_flow(data)
+    # multicut_flow(data)
+    # multicut_compare_tradition(data)
 
-    # f4_11()
-    # f3_1()
-    # data3.put_index(24)
-
-    # plt.figure()
-    # data1.zero_line()
-    # data1.boundary_line(1/3)
-
-    # data1.etabs_demand_line('gray')
-    # data1.add_ld_line('gray', label='Consider $l_d$')
-    # data2.add_ld_line('gray', label='Consider $l_d$')
-
-    # line2 = data1.rebar_line('orange', '傳統斷筋')
-    # line3 = data1.rebar_line('blue', '多點斷筋')
-    # line2 = data2.rebar_line('red', '傳統斷筋')
-    # line3 = data2.rebar_line('green', '多點斷筋')
-
-    # plt.xlabel('Length(m)')
-    # plt.ylabel('As($m^2$)')
-    # plt.tight_layout()
+    # v_workflow(data)
+    # v_multicut_compare_tradition(data)
 
     # to_excel_orderby_effect(
-    #     data1,
+    #     data,
     #     f'{script_dir}/../data/20190520 104202 SmartCut 欣詮'
     # )
-
-    # etabs_to_addedld_sol(data1)
-    # v_multicut_compare_tradition(data1)
-    # v_multicut_compare_tradition(data2)
-    # v_multicut_compare_tradition(data3)
-    # tradition_flow(data1)
-    # multicut_flow(data1)
-    # multicut_compare_tradition(data1)
-    # tradition_flow(data2)
-
-    # etabs_to_addedld_sol(data2)
-    # multicut_compare_tradition(data2)
-    # multicut_compare_tradition(data3)
-
-    # v_workflow(data1)
 
     plt.show()
 
