8615eeab97b588b6c22b1e3ae0a0ef056bb8e4d3
skyran1278 <skyran1278@gmail.com>
2019-06-15 23:49:19 +0800

    4-8

--- EvaluationReport/evaluation/app.py
+++ EvaluationReport/evaluation/app.py
@@ -253,23 +253,22 @@ def f4_8():
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     data1 = PlotDesign(
-        f'{script_dir}/../data/MidSeismic 4Floor 6M.xlsx')
+        'D:/GitHub/thesis/Models/MidSeismic 4Floor 6M/20190614 154236 Cut 2.xlsx')
     data2 = PlotDesign(
-        f'{script_dir}/../data/MidSeismic 4Floor 9M.xlsx')
+        'D:/GitHub/thesis/Models/MidSeismic 4Floor 9M/20190614 154307 Cut 2.xlsx')
     data3 = PlotDesign(
-        f'{script_dir}/../data/MidSeismic 4Floor 12M.xlsx')
+        'D:/GitHub/thesis/Models/MidSeismic 4Floor 12M/20190614 154334 Cut 2.xlsx')
 
     data1.put_index(24)
     data2.put_index(24)
     data3.put_index(24)
 
-    plt.figure(figsize=(6.4, 8))
+    plt.figure(figsize=(6.4, 9.6))
 
     plt.subplot(3, 1, 1)
     data1.zero_line()
 
     line1 = data1.etabs_demand_line('blue', top=False)
-    line4 = data1.add_ld_line('orange', top=False)
 
     line2 = data1.rebar_line('red', '傳統斷筋', top=False)
     line3 = data1.rebar_line('green', '多點斷筋', top=False)
@@ -277,18 +276,17 @@ def f4_8():
     plt.ylabel('As($m^2$)')
     plt.xlabel('Length(m)')
     plt.legend(
-        (line1, line4, line2, line3),
-        ('Demand', 'Consider Ld', 'Tradition', 'Multi-Cut'),
-        loc='best'
+        (line1, line2, line3),
+        ('Demand', 'Tradition', 'Multi-Cut'),
+        loc='upper left'
     )
-    plt.title('(a)Beam Length 6m Case')
+    plt.title('(a)Mid Seismic Beam Length 6m Case')
     plt.grid(True, which='both', linestyle=':')
 
     plt.subplot(3, 1, 2)
     data2.zero_line()
 
     line1 = data2.etabs_demand_line('blue', top=False)
-    line4 = data2.add_ld_line('orange', top=False)
 
     line2 = data2.rebar_line('red', '傳統斷筋', top=False)
     line3 = data2.rebar_line('green', '多點斷筋', top=False)
@@ -296,18 +294,18 @@ def f4_8():
     plt.xlabel('Length(m)')
     plt.ylabel('As($m^2$)')
     plt.legend(
-        (line1, line4, line2, line3),
-        ('Demand', 'Consider Ld', 'Tradition', 'Multi-Cut'),
-        loc='best'
+        (line1, line2, line3),
+        ('Demand', 'Tradition', 'Multi-Cut'),
+        loc='upper left'
     )
-    plt.title('(b)Beam Length 9m Case')
+    plt.title('(b)Mid Seismic Beam Length 9m Case')
+    plt.grid(True, which='both', linestyle=':')
 
     plt.subplot(3, 1, 3)
     data3.zero_line()
     plt.grid(True, which='both', linestyle=':')
 
     line1 = data3.etabs_demand_line('blue', top=False)
-    line4 = data3.add_ld_line('orange', top=False)
 
     line2 = data3.rebar_line('red', '傳統斷筋', top=False)
     line3 = data3.rebar_line('green', '多點斷筋', top=False)
@@ -315,11 +313,11 @@ def f4_8():
     plt.xlabel('Length(m)')
     plt.ylabel('As($m^2$)')
     plt.legend(
-        (line1, line4, line2, line3),
-        ('Demand', 'Consider Ld', 'Tradition', 'Multi-Cut'),
-        loc='best'
+        (line1, line2, line3),
+        ('Demand', 'Tradition', 'Multi-Cut'),
+        loc='upper left'
     )
-    plt.title('(c)Beam Length 12m Case')
+    plt.title('(c)Mid Seismic Beam Length 12m Case')
     plt.grid(True, which='both', linestyle=':')
 
     plt.tight_layout()
